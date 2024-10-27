fa8260afcfca1353ec760768145b8cf324e9699e
Tolga Ören <orentolga@hotmail.com>
2024-10-28 02:22:38 +0300

    first week done

--- app/src/main/java/com/toren/hackathon24educationproject/presentation/sign_up/SignUpScreen.kt
+++ app/src/main/java/com/toren/hackathon24educationproject/presentation/sign_up/SignUpScreen.kt
@@ -1,19 +1,14 @@
 package com.toren.hackathon24educationproject.presentation.sign_up
 
 import android.widget.Toast
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
